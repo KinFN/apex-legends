b04ebba95cc68295741fa9dd172d64f5d011b1bc
KinFN <56640971+KinFN@users.noreply.github.com>
2022-09-02 12:17:49 -0400

    Update script.js

--- login/script.js
+++ login/script.js
@@ -1,2 +1,2 @@
 function onSubmit() {
-         if (document.getElementById('password').value == '1234') {window.button2.style.display="block"; }else{ alert('Access Denied, Please try again');}
+         if (document.getElementById('password').value == 'testing') {window.button2.style.display="block"; }else{ alert('Access Denied, Please try again');}
