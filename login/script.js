1fdc658ace318c1d6c7a52de80caf7f64c023477
KinFN <56640971+KinFN@users.noreply.github.com>
2022-09-02 13:57:26 -0400

    Delete script.js

--- login/script.js
+++
@@ -1,6 +0,0 @@
-function onSubmit() {
-         if (document.getElementById('password').value == 'oskie222','gaminglover123','biggothgirls&&') {window.button2.style.display="block"; }else{ alert('Username and/or password was incprrect');}
-     }
-     
-
-     
