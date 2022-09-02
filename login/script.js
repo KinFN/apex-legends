f4e62d3389cecc1c0c693d5f5cc2b4af541807b0
KinFN <56640971+KinFN@users.noreply.github.com>
2022-09-02 13:50:35 -0400

    Update script.js

--- login/script.js
+++ login/script.js
@@ -1,16 +1,6 @@
 function onSubmit() {
-         if (document.getElementById('password').value == 'oskie222','gaminglover123') {window.button2.style.display="block"; }else{ alert('Username and/or password was incprrect');}
+         if (document.getElementById('password').value == 'oskie222','gaminglover123','biggothgirls&&') {window.button2.style.display="block"; }else{ alert('Username and/or password was incprrect');}
      }
      
 
      
-     
-     
-// In the code above, the ".value" sets the passcode. 
-// If you change it to a longer number, make sure you change 
-// the "maxlength" on the index.htm page to match. 
-
-// You can also change the web page in the "href" to another page. 
-// Simply replace "http://google.com" with your desired page. 
-
-// You can also change the "alert" text above to what you want to say.
